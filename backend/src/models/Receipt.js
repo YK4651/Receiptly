2b1bfc267d88155b42c81bedf94eb092eaa717ae
Ryan David <rdavid12@mylangara.ca>
2025-02-20 00:07:41 -0800

    created the api for receipt parsing on backend
    api functionality created to parse the image on backend as well as to categorize the receipt, used the POC that was created by Yoshiki as basis of the methods

---
+++ backend/src/models/Receipt.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const receiptSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    description: "The ID of the user who owns this receipt"
+  },
+  receiptData: {
+    type: Array,
+    required: true,
+    description: "The data extracted from the receipt"
+  },
+  imageUrls: {
+    type: Array,
+    required: true,
+    description: "The files associated with the receipt"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    description: "When the receipt was created"
+  }
+});
+
+const Receipt = mongoose.model('Receipt', receiptSchema);
+
+module.exports = Receipt;
