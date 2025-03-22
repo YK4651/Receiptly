754d9cfd5993e1ffe61bb813440551e1a785683a
Ryan David <rdavid12@mylangara.ca>
2025-03-22 11:48:00 -0700

    changes to dashboard page

--- frontend/src/components/pages/Dashboard.jsx
+++ frontend/src/components/pages/Dashboard.jsx
@@ -3,16 +3,16 @@ import React, { useState, useEffect } from "react";
 import { fetchReceipts } from "../../api/receipts";
 import { toast } from "react-toastify";
 import {
-	Chart as ChartJS,
-	CategoryScale,
-	LinearScale,
-	BarElement,
-	Title,
-	Tooltip,
-	Legend,
-	PointElement,
-	LineElement,
-	ArcElement,
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement,
+  ArcElement,
 } from "chart.js";
 import LineChart from "./Reports/Charts/LineChart";
 import DoughnutChart from "./Reports/Charts/DoughnutChart";
@@ -20,43 +20,43 @@ import GaugeChart from "./Reports/Charts/GaugeChart";
 import BarChart from "./Reports/Charts/BarChart";
 import HelpToolTip from "../common/HelpToolTip";
 import {
-	burnRateMessage,
-	runwayMessage,
-	grossMarginMessage,
+  burnRateMessage,
+  runwayMessage,
+  grossMarginMessage,
 } from "../common/ToolTipMessage";
 import GetStartedBanner from "../common/GetStartedBanner";
 import DashboardCards from "../common/DashboardCards";
 import Button from "../common/Button";
 
 ChartJS.register(
-	CategoryScale,
-	LinearScale,
-	BarElement,
-	Title,
-	Tooltip,
-	Legend,
-	PointElement,
-	LineElement,
-	ArcElement
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement,
+  ArcElement
 );
 
 const Dashboard = () => {
   const [selectedCategory, setSelectedCategory] = useState("Financial");
   const categoryCharts = {
-		Financial: [
+    Financial: [
       { title: "Runway", component: <GaugeChart />, message: runwayMessage() },
-			{
-				title: "Gross Margin",
-				component: <DoughnutChart />,
-				message: grossMarginMessage(),
-			},
-			{
-				title: "Burn Rate",
-				component: <LineChart />,
-				message: burnRateMessage(),
-			},
-		]
-	};
+      {
+        title: "Gross Margin",
+        component: <DoughnutChart />,
+        message: grossMarginMessage(),
+      },
+      {
+        title: "Burn Rate",
+        component: <LineChart />,
+        message: burnRateMessage(),
+      },
+    ]
+  };
 
   return (
     <div>
@@ -69,23 +69,23 @@ const Dashboard = () => {
       <GetStartedBanner></GetStartedBanner>
       <DashboardCards></DashboardCards>
       <div id='report-container' className='p-4 bg-white w-full'>
-				<div className='grid grid-cols-2 gap-4'>
-					{categoryCharts[selectedCategory]?.map((chart, index) => (
-						<div
-							key={index}
-							className={`border border-gray-200 rounded-xl ${
-								chart.component.type === BarChart ? "col-span-2" : ""
-							} ${chart.component.type === LineChart ? "col-span-2" : ""}`}
-						>
-							<h3 className='text-md bg-gray-200/40 p-3 pl-6'>
-								<span className='align-[4px]'>{chart.title}</span>
-								<HelpToolTip message={`${chart.message}`} />
-							</h3>
-							<div className='px-4 py-8'>{chart.component}</div>
-						</div>
-					))}
-				</div>
-			</div>
+        <div className='grid grid-cols-2 gap-4'>
+          {categoryCharts[selectedCategory]?.map((chart, index) => (
+            <div
+              key={index}
+              className={`border border-gray-200 rounded-xl ${
+                chart.component.type === BarChart ? "col-span-2" : ""
+              } ${chart.component.type === LineChart ? "col-span-2" : ""}`}
+            >
+              <h3 className='text-md bg-gray-200/40 p-3 pl-6'>
+                <span className='align-[4px]'>{chart.title}</span>
+                <HelpToolTip message={`${chart.message}`} />
+              </h3>
+              <div className='px-4 py-8'>{chart.component}</div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
