2d457f3445c195629600d2b7c6244457bd181f34
Ryan David <rdavid12@mylangara.ca>
2025-04-02 19:36:50 -0700

    updates merged from main to development

--- frontend/src/components/common/GetStartedBanner.jsx
+++ frontend/src/components/common/GetStartedBanner.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
-const GetStartedBanner = ({ onSkip, onCompleteProfile }) => {
+const GetStartedBanner = () => {
+  const [showBanner, setShowBanner] = useState(true);
+  const navigate = useNavigate();
+
+  const handleCompleteProfile = () => {
+    navigate('/settings');
+  };
+
+  const handleSkipForLater = () => {
+    setShowBanner(false);
+  };
+
   return (
     showBanner && ( <div className="w-full bg-[#F5F5FF] p-5 rounded-lg flex justify-center mb-6">
       <div className="max-w-6xl w-full mx-auto flex flex-col md:flex-row items-center justify-between">
@@ -16,15 +27,14 @@ const GetStartedBanner = ({ onSkip, onCompleteProfile }) => {
           </p>
           <div className="mt-4 flex space-x-4">
             <button
-              className="bg-[#2E39E6] text-white px-6 py-2 rounded-md"
-              onClick={onCompleteProfile}
+              onClick={handleCompleteProfile}
+              className="bg-[#2E39E6] text-white px-6 py-2 rounded-md hover:bg-transparent hover:text-[#2E39E6] border border-[#2E39E6] cursor-pointer transition duration-300"
             >
               Complete Profile
             </button>
-            <button
-              className="border border-[#2E39E6] text-[#2E39E6] px-6 py-2 rounded-md"
-              onClick={onSkip}
-            >
+            <button 
+              onClick={handleSkipForLater}
+              className="border border-[#2E39E6] text-[#2E39E6] px-6 py-2 rounded-md bg-transparent hover:text-white hover:bg-[#2E39E6] cursor-pointer transition duration-300">
               Skip for Later
             </button>
           </div>
